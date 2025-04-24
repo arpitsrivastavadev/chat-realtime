6502be93bf978d459072d4cf8a8f23285dab2460
Arpit Srivastava <arpitsrivastavadev11@gmail.com>
2025-04-24 22:51:39 +0530

    Add tailwindcss, react-icons, and react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
     return (
         <>
-            <h1>Hello, world!</h1>
+            <Routes>
+                <Route path="/" element={<h1>Hello, world!</h1>} />
+                <Route path="*" element={<h1>404, not found.</h1>} />
+            </Routes>
         </>
     )
 }
